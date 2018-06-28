125f956ed78f743c63f3d3805b1049c63828e2e1
Isaac Hier <isaachier@gmail.com>
2018-06-27 20:12:54 -0400

    Improve style

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,15 @@
+const std = @import("std");
+
 const c = @import("c.zig");
 
 const ErrorSet = error{SDLError};
 
 pub fn main() !void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO | c.SDL_INIT_AUDIO) != 0) {
-        c.SDL_Log(c"Unable to initialize SDL: %s", c.SDL_GetError());
+        std.debug.warn("Unable to initialize SDL: {s}", c.SDL_GetError());
         return ErrorSet.SDLError;
     }
+    defer c.SDL_Quit();
 
     const screen_width = 640;
     const screen_height = 480;
@@ -20,8 +23,11 @@ pub fn main() !void {
         screen_height,
         c.SDL_WINDOW_SHOWN,
     );
+    if (window == null) {
+        std.debug.warn("Cannot create window: {s}", c.SDL_GetError());
+        return ErrorSet.SDLError;
+    }
+    defer c.SDL_DestroyWindow(window);
     const screen = c.SDL_GetWindowSurface(window);
     c.SDL_Delay(2000);
-    c.SDL_DestroyWindow(window);
-    c.SDL_Quit();
 }
