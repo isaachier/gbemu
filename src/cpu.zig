3d44a71c10a676eaac09ec588665d15a503ec352
Isaac Hier <isaachier@gmail.com>
2018-07-02 09:07:40 -0400

    Implement a few 8 bit loads

--- src/cpu.zig
+++ src/cpu.zig
@@ -124,30 +124,148 @@ test "Registers" {
     std.debug.assert(registers.l() == 0x55);
 }
 
-pub const CPU = struct {
+pub const Memory = struct {
+    const memory_len = 0xffff - 0x2000;
+
     allocator: *std.mem.Allocator,
-    registers: Registers,
     memory: []u8,
 
-    pub fn init(allocator: *std.mem.Allocator) !CPU {
-        const memory_len = 0xffff;
-        return CPU{
+    pub fn init(allocator: *std.mem.Allocator) !Memory {
+        return Memory{
             .allocator = allocator,
-            .registers = undefined,
             .memory = try allocator.alloc(u8, memory_len),
         };
     }
 
-    pub fn deinit(self: *CPU) void {
+    pub fn deinit(self: *Memory) void {
         self.allocator.free(self.memory);
     }
 
-    pub fn execute(self: *CPU) void {
-        // TODO
+    fn internalIndex(index: u16) u16 {
+        if (index < 0xe000) {
+            return index;
+        }
+        return index - 0x2000;
+    }
+
+    pub fn get(self: *const Memory, index: u16) u8 {
+        return self.memory[internalIndex(index)];
+    }
+
+    pub fn set(self: *Memory, index: u16, value: u8) void {
+        self.memory[internalIndex(index)] = value;
+    }
+
+};
+
+pub const ErrorSet = error{ InvalidInstruction };
+
+pub const CPU = struct {
+    registers: Registers,
+    memory: Memory,
+
+    pub fn init(allocator: *std.mem.Allocator) !CPU {
+        return CPU{
+            .registers = undefined,
+            .memory = try Memory.init(allocator),
+        };
+    }
+
+    pub fn deinit(self: *CPU) void {
+        self.memory.deinit();
+    }
+
+    pub fn execute(self: *CPU) !void {
+        switch (self.memory.get(self.registers.pc)) {
+            // 8-Bit Loads
+            // Immediate value loads
+            0x06 => {
+                // LD B,n
+                self.registers.setB(self.memory.get(self.registers.pc + 1));
+                self.registers.pc += 2;
+            },
+            0x0E => {
+                // LD C,n
+                self.registers.setC(self.memory.get(self.registers.pc + 1));
+                self.registers.pc += 2;
+            },
+            0x16 => {
+                // LD D,n
+                self.registers.setD(self.memory.get(self.registers.pc + 1));
+                self.registers.pc += 2;
+            },
+            0x1E => {
+                // LD E,n
+                self.registers.setE(self.memory.get(self.registers.pc + 1));
+                self.registers.pc += 2;
+            },
+            0x26 => {
+                // LD H,n
+                self.registers.setH(self.memory.get(self.registers.pc + 1));
+                self.registers.pc += 2;
+            },
+            0x2E => {
+                // LD L,n
+                self.registers.setL(self.memory.get(self.registers.pc + 1));
+                self.registers.pc += 2;
+            },
+            // Register loads
+            0x7F => {
+                // LD A,A
+                self.registers.setA(self.registers.a());
+                self.registers.pc += 1;
+            },
+            0x78 => {
+                // LD A,B
+                self.registers.setA(self.registers.b());
+                self.registers.pc += 1;
+            },
+            0x79 => {
+                // LD A,C
+                self.registers.setA(self.registers.c());
+                self.registers.pc += 1;
+            },
+            0x7A => {
+                // LD A,D
+                self.registers.setA(self.registers.d());
+                self.registers.pc += 1;
+            },
+            0x7B => {
+                // LD A,E
+                self.registers.setA(self.registers.e());
+                self.registers.pc += 1;
+            },
+            0x7C => {
+                // LD A,H
+                self.registers.setA(self.registers.h());
+                self.registers.pc += 1;
+            },
+            0x7D => {
+                // LD A,L
+                self.registers.setA(self.registers.l());
+                self.registers.pc += 1;
+            },
+            0x7E => {
+                // LD A,(HL)
+                self.registers.setA(self.memory.get(self.registers.hl));
+                self.registers.pc += 1;
+            },
+            else => {
+                return ErrorSet.InvalidInstruction;
+            },
+        }
     }
 };
 
 test "CPU" {
     var cpu = try CPU.init(std.debug.global_allocator);
+    cpu.registers.pc = 0;
+    cpu.registers.hl = 0x55;
+    cpu.memory.set(0x0, 0x7E);
+    cpu.memory.set(0x55, 0x20);
+    try cpu.execute();
+    std.debug.warn("a = {x}\n", cpu.registers.a());
+    std.debug.assert(cpu.registers.a() == 0x20);
+    std.debug.assert(cpu.registers.pc == 1);
     cpu.deinit();
 }
